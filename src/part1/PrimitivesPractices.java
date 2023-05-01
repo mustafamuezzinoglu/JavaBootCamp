a8a7f6cbbf40a3f78c61dabefda9e6694344b793
Mustafa <mustafamuezzinoglu@gmail.com>
2023-05-01 17:55:20 +0300

    first initial

---
+++ src/part1/PrimitivesPractices.java
@@ -0,0 +1,22 @@
+package part1;
+
+public class PrimitivesPractices {
+
+    public static void main(String[] args) {
+
+        // DataTYpe var;
+        int a ;
+        a=10;
+        System.out.println(a);
+
+        double b = (double) a;
+        double z = a ; //implicit casting no need to write anything
+        System.out.println(b); //it gives double 10.0
+        System.out.println(z); //it gives double 10.0
+
+        //todo;
+
+
+
+    }
+}
---
+++ src/part1/WrapperClasses.java
@@ -0,0 +1,21 @@
+package part1;
+
+public class WrapperClasses {
+
+    public static void main(String[] args) {
+
+        char a = 'A';
+
+        Character b = (Character) a;
+
+        int c = 25;
+
+        Integer d = c;
+
+        boolean l = false;
+
+        boolean k = l;
+
+
+    }
+}
